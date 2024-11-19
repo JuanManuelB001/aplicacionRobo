f28931a5769ee567c444a8d24af0fd461306e614
Juan Manuel Bolivar <jbolivarm2@ucentral.edu.co>
2024-11-19 09:07:28 -0500

    Agregando logica fragment Bluetooth

--- app/src/main/java/com/example/aplicacionrobo/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/aplicacionrobo/ui/dashboard/DashboardFragment.java
@@ -1,19 +1,56 @@
 package com.example.aplicacionrobo.ui.dashboard;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.aplicacionrobo.databinding.FragmentDashboardBinding;
 
-public class DashboardFragment extends Fragment {
 
+//BLUETOOTH ADAPTER
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+
+public class DashboardFragment extends Fragment {
+    private static final String TAG = "DashboardFragment";
+    private static final UUID BT_MODULE_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final int REQUEST_BLUETOOTH_CONNECT_PERMISSION = 3;
+    private static final int REQUEST_FINE_LOCATION_PERMISSION = 2;
+    private BluetoothAdapter mBtAdapter;
+    private BluetoothSocket btSocket;
+    private BluetoothDevice DispositivoSeleccionado;
+    private ConnectedThread MyConexionBT;
+    private ArrayList<String> mNameDevices = new ArrayList<>();
+    private ArrayAdapter<String> deviceAdapter;
     private FragmentDashboardBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -21,17 +58,175 @@ public class DashboardFragment extends Fragment {
         DashboardViewModel dashboardViewModel =
                 new ViewModelProvider(this).get(DashboardViewModel.class);
 
+        // Solicitar permisos
+        requestBluetoothConnectPermission();
+        requestLocationPermission();
+
+        // Inflar la vista usando View Binding
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        // Configuración del ArrayAdapter para el Spinner
+        deviceAdapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, mNameDevices);
+        deviceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.IdDispEncontrados.setAdapter(deviceAdapter);
+
+        // Configuración de eventos de clic para los botones
+        binding.IdBtnConectar.setOnClickListener(view -> ConectarDispBT());
+        binding.IdBtnDesconectar.setOnClickListener(view -> desconectarBluetooth());
+        binding.idBtnBuscar.setOnClickListener(view -> DispositivosVinculados());
+
+        binding.IdBtnLuz1on.setOnClickListener(view ->
+                Toast.makeText(getActivity(), "SE HA PRESIONADO EL BOTON LUZ 1 ON", Toast.LENGTH_SHORT).show());
+
+        binding.IdBtnLuz1off.setOnClickListener(view ->
+                Toast.makeText(getActivity(), "SE HA PRESIONADO EL BOTON LUZ 1 OFF", Toast.LENGTH_SHORT).show());
+
+        binding.IdBtnLuz2on.setOnClickListener(view ->
+                Toast.makeText(getActivity(), "SE HA PRESIONADO EL BOTON LUZ 2 ON", Toast.LENGTH_SHORT).show());
+
+        binding.IdBtnLuz2off.setOnClickListener(view ->
+                Toast.makeText(getActivity(), "SE HA PRESIONADO EL BOTON LUZ 2 OFF", Toast.LENGTH_SHORT).show());
+
         return root;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
+        binding = null; // Liberamos el binding cuando la vista es destruida
+    }
+
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == DashboardFragment.REQUEST_ENABLE_BT) {
+                        Log.d(TAG, "ACTIVIDAD REGISTRADA");
+                    }
+                }
+            });
+
+    private void DispositivosVinculados() {
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBtAdapter == null) {
+            showToast("Bluetooth no disponible en este dispositivo.");
+            return;
+        }
+
+        if (!mBtAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            someActivityResultLauncher.launch(enableBtIntent);
+        }
+
+        binding.IdDispEncontrados.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                DispositivoSeleccionado = getBluetoothDeviceByName(mNameDevices.get(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                DispositivoSeleccionado = null;
+            }
+        });
+
+        Set<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            mNameDevices.clear();
+            for (BluetoothDevice device : pairedDevices) {
+                mNameDevices.add(device.getName());
+            }
+            deviceAdapter.notifyDataSetChanged();
+        } else {
+            showToast("No hay dispositivos Bluetooth emparejados.");
+        }
+    }
+
+    private void requestLocationPermission() {
+        ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION_PERMISSION);
+    }
+
+    private void requestBluetoothConnectPermission() {
+        ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.BLUETOOTH_CONNECT}, REQUEST_BLUETOOTH_CONNECT_PERMISSION);
+    }
+
+    private BluetoothDevice getBluetoothDeviceByName(String name) {
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, " ----->>>>> ActivityCompat.checkSelfPermission");
+        }
+        Set<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
+        for (BluetoothDevice device : pairedDevices) {
+            if (device.getName().equals(name)) {
+                return device;
+            }
+        }
+        return null;
+    }
+
+    private void ConectarDispBT() {
+        if (DispositivoSeleccionado == null) {
+            showToast("Selecciona un dispositivo Bluetooth.");
+            return;
+        }
+
+        try {
+            if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            btSocket = DispositivoSeleccionado.createRfcommSocketToServiceRecord(BT_MODULE_UUID);
+            btSocket.connect();
+            MyConexionBT = new ConnectedThread(btSocket);
+            MyConexionBT.start();
+            showToast("Conexión exitosa.");
+        } catch (IOException e) {
+            showToast("Error al conectar con el dispositivo.");
+        }
+    }
+
+    private void desconectarBluetooth() {
+        if (btSocket != null) {
+            try {
+                btSocket.close();
+                showToast("Desconexión exitosa.");
+            } catch (IOException e) {
+                showToast("Error al desconectar el dispositivo.");
+            }
+        }
+    }
+
+    private class ConnectedThread extends Thread {
+        private final OutputStream mmOutStream;
+
+        ConnectedThread(BluetoothSocket socket) {
+            OutputStream tmpOut = null;
+
+            try {
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+                showToast("Error al crear el flujo de datos.");
+            }
+
+            mmOutStream = tmpOut;
+        }
+
+        public void write(char input) {
+            try {
+                mmOutStream.write((byte) input);
+            } catch (IOException e) {
+                showToast("La Conexión falló.");
+            }
+        }
+    }
+
+    private void showToast(final String message) {
+        if (getActivity() != null) {
+            getActivity().runOnUiThread(() ->
+                    Toast.makeText(getActivity().getApplicationContext(), message, Toast.LENGTH_SHORT).show());
+        }
     }
 }
--- app/src/main/java/com/example/aplicacionrobo/ui/home/mapaFragment.java
+++
@@ -1,8 +0,0 @@
-package com.example.aplicacionrobo.ui.home;
-
-import androidx.fragment.app.Fragment;
-
-public class mapaFragment extends Fragment {
-
-
-}
